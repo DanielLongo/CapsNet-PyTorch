40f2ef7be7dabc9c834abf5822dea1ae37b4598d
Daniel Longo <DanielLongo@daniels-mbp-3.lan>
2018-11-23 19:19:27 -0800

    layers

--- CapsuleLayers.py
+++ CapsuleLayers.py
@@ -83,24 +83,24 @@ class DenseCapsule(nn.Module):
         return torch.squeeze(outputs, dim=-2)
 
 
-class PrimaryCapsule(nn.Module):
-    """
-    Apply Conv2D with `out_channels` and then reshape to get capsules
-    :param in_channels: input channels
-    :param out_channels: output channels
-    :param dim_caps: dimension of capsule
-    :param kernel_size: kernel size
-    :return: output tensor, size=[batch, num_caps, dim_caps]
-    """
-    def __init__(self, in_channels, out_channels, dim_caps, kernel_size, stride=1, padding=0):
-        super(PrimaryCapsule, self).__init__()
-        self.dim_caps = dim_caps
-        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+# class PrimaryCapsule(nn.Module):
+#     """
+#     Apply Conv2D with `out_channels` and then reshape to get capsules
+#     :param in_channels: input channels
+#     :param out_channels: output channels
+#     :param dim_caps: dimension of capsule
+#     :param kernel_size: kernel size
+#     :return: output tensor, size=[batch, num_caps, dim_caps]
+#     """
+#     def __init__(self, in_channels, out_channels, dim_caps, kernel_size, stride=1, padding=0):
+#         super(PrimaryCapsule, self).__init__()
+#         self.dim_caps = dim_caps
+#         self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
 
-    def forward(self, x):
-        outputs = self.conv2d(x)
-        outputs = outputs.view(x.size(0), -1, self.dim_caps)
-        return squash(outputs)
+#     def forward(self, x):
+#         outputs = self.conv2d(x)
+#         outputs = outputs.view(x.size(0), -1, self.dim_caps)
+#         return squash(outputs)
 
 # '''
 # Pytorch Implementation of "Dynamic Routing Between Capsules"
@@ -122,33 +122,24 @@ class PrimaryCapsule(nn.Module):
 # 	out = scale * x / torch.sqrt(squared_norm)
 # 	return out
 
-# class PrimaryCapsule(nn.Module):
-# 	def __init__(self):		
-# 		super(PrimaryCapsule, self).__init__()
-# 		self.conv1 = nn.Sequential(
-# 			nn.Conv2d(1, 256, kernel_size=9, stride=1, padding=0),
-# 			nn.ReLU()
-# 		)
-		
-# 		self.conv2 = nn.Sequential(
-# 			nn.Conv2d(256, caps1_n_maps * caps1_n_dims, kernel_size=9, stride=2, padding=0),
-# 			nn.ReLU()
-# 		)
+class PrimaryCapsule(nn.Module):
+	def __init__(self):		
+		super(PrimaryCapsule, self, num_maps=32, num_dims=8).__init__()
+		self.num_maps = num_maps
+		self.num_caps = 6 * 6 * self.num_maps
+		self.num_dims = num_dims
+		self.conv1 = nn.Sequential(
+			nn.Conv2d(256, self.num_maps * self.num_dims, kernel_size=9, stride=2, padding=0),
+			nn.ReLU()
+		)
 
-# 	def forward(self, x):
-# 		# 28, 28, 1
-# 		print("x", x.shape)
-# 		out = self.conv1(x)
-# 		print("out", out.shape)
-# 		# 20, 20, 256
-# 		out = self.conv2(out)
-# 		print("out", out.shape)
-# 		# 6, 6, 256
-# 		out = out.view(-1, caps1_n_caps, caps1_n_dims)
-# 		print("out", out.shape)
-# 		out = squash(out)
-# 		print("out", out.shape)
-# 		return out
+	def forward(self, x):
+		# 20, 20, 256
+		out = self.conv1(x)
+		# 6, 6, 256
+		out = out.view(-1, self.num_caps, self.num_dims)
+		out = squash(out)
+		return out
 
 # caps1_n_maps = 32
 # caps1_n_caps = caps1_n_maps * 6 * 6 #1152 primary capsules
